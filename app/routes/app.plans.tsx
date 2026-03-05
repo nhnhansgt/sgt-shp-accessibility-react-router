e024f4d9144953f68952b5ac2e567dabca75fbd7
nhnhansgt <nhnhan@saigon-tech.vn>
2026-03-05 10:12:54 +0700

    feat: add Year End Sale banner to Plans screen

--- app/routes/app.plans.tsx
+++ app/routes/app.plans.tsx
@@ -46,7 +46,22 @@ export default function Plans() {
 
   return (
     <s-page heading="Choose your plan">
-      {/* TODO: Add Year End Sale Banner */}
+      {isYearEndSale && (
+        <s-section>
+          <s-box
+            padding="base"
+            background="subdued"
+            borderRadius="base"
+          >
+            <s-stack direction="inline" gap="base">
+              <span>🎉</span>
+              <s-text>
+                Year End Sale! Get <strong>{saleDays} days</strong> free trial instead of 14!
+              </s-text>
+            </s-stack>
+          </s-box>
+        </s-section>
+      )}
 
       {/* TODO: Add Plans Grid */}
     </s-page>
